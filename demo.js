198eb164a789b343e36cfd6fe5e4dc99bf76eccf
hjm1233 <2093230346@qq.com>
2020-11-07 10:08:46 +0800

    更改文件实现两个div弹窗效果

--- demo.js
+++ demo.js
@@ -1,6 +1,9 @@
 window.onload=function(){
-    let box=document.getElementsByClassName('box')[0];
-    box.addEventListener('click',()=>{
-        alert('这是测试代码');
+    let box=document.getElementsByClassName('box');
+    for(var item in box){
+        let a=item;
+        box[item].addEventListener('click',()=>{
+        alert('这是测试代码'+(Number(a)+1));
     })
+    }
 }
